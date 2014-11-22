4f02b8b794e2ca818669117ed0822e079c63dc3a
ramesh bommakanti <bramesh.pgp@gmail.com>
2014-11-22 14:54:11 +0530

    this first program

---
+++ testjava/src/com/venkat/bank/FirstProgram.java
@@ -0,0 +1,15 @@
+package com.venkat.bank;
+
+public class FirstProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubt
+		
+		System.out.println("welcometo java");
+
+	}
+
+}
